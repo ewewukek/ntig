ef0e510447dbb764cfa108388fdd89f67011bd6b
ewewukek <ewewukek@gmail.com>
2024-09-12 02:33:20 +0300

    parse refs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Interactive git log with commit preview. Inspired by [Tig](https://jonas.github.io/tig/).
 
-Requires `termios` package.
+Requires `termios` package and ANSI escape sequences support.
 
 ![](screenshot.png?raw=true)
 
@@ -8,10 +8,17 @@ Requires `termios` package.
 options:
   --ntig-hash       hash color (default: yellow)
   --ntig-date       date color (default: default)
+  --ntig-date-fmt   date format (default: %d %b %H:%M)
   --ntig-author     author name color (default: cyan)
   --ntig-node       graph node color (default: bright-yellow)
-  --ntig-date-fmt   date format (default: %d %b %H:%M)
-  --ntig-log        log format (default: {hash} {date} {author} {graph} {message})
+  --ntig-head       HEAD color (default: bright-cyan)
+  --ntig-branch     local branch color (default: bright-green)
+  --ntig-remote     remote branch color (default: bright-red)
+  --ntig-tag        tag color (default: bright-yellow)
+  --ntig-stash      stash color (default: bright-blue)
+  --ntig-refs-bold  show refs in bold (default: False)
+  --ntig-decorate   decorate refs (default: False)
+  --ntig-log        log format (default: {hash} {date} {author} {graph} {refs}{message})
   --ntig-pager      pager to pipe output of git show (default: none)
 
 other options will be passed to git log
--- ntig.py
+++ ntig.py
@@ -51,6 +51,12 @@ def load_log(cfg):
             '--pretty=format:%ad %aN %h %s',
             '--date=format:%s'
         ])
+
+        if '{refs}' in cfg['log_fmt']:
+            refs_data = subprocess.check_output(['git', 'show-ref', '--head'])
+        else:
+            refs_data = b''
+
     except subprocess.CalledProcessError as e:
         if e.output:
             print(e.output)
@@ -71,6 +77,7 @@ def load_log(cfg):
             'date': '',
             'author': '',
             'hash': '',
+            'refs': '',
             'message': '',
         }
         if m := log_line_re.match(line):
@@ -85,7 +92,7 @@ def load_log(cfg):
 
         g = g.strip()
         g = brighten(g)
-        if cfg['node_color'] and cfg['node_color'] != COLORS['default']:
+        if cfg['node_color'] != COLORS['default']:
             g = g.replace('*', cfg['node_color'] + '*' + COLORS['default'])
         g = g.replace('\033[m', '\033[39m')
         commit['graph'] = g
@@ -95,10 +102,71 @@ def load_log(cfg):
 
         log.append(commit)
 
+    refs = {}
+    refs_line_re = re.compile(r'^([0-9a-f]{40,}) (.*)$')
+    for line in refs_data.splitlines():
+        line = line.decode('utf8')
+
+        m = refs_line_re.match(line)
+        h = m.group(1)[0:lengths['hash']]
+        if h not in refs:
+            refs[h] = {}
+
+        ref = m.group(2)
+        if ref == 'HEAD':
+            if cfg['decorate']:
+                ref = '[' + ref + ']'
+            refs[h]['head'] = [ref]
+
+        elif ref[0:11] == 'refs/heads/':
+            if 'branch' not in refs[h]:
+                refs[h]['branch'] = []
+            ref = ref[11:]
+            if cfg['decorate']:
+                ref = '[' + ref + ']'
+            refs[h]['branch'].append(ref)
+
+        elif ref[0:13] == 'refs/remotes/':
+            if 'remote' not in refs[h]:
+                refs[h]['remote'] = []
+            ref = ref[13:]
+            if cfg['decorate']:
+                ref = '{' + ref + '}'
+            refs[h]['remote'].append(ref)
+
+        elif ref[0:10] == 'refs/tags/':
+            if 'tag' not in refs[h]:
+                refs[h]['tag'] = []
+            ref = ref[10:]
+            if cfg['decorate']:
+                ref = '<' + ref + '>'
+            refs[h]['tag'].append(ref)
+
+        elif ref == 'refs/stash':
+            if cfg['decorate']:
+                ref = '[' + ref + ']'
+            refs[h]['stash'] = [ref]
+
     for commit in log:
+        if commit['hash'] in refs:
+            ref = refs[commit['hash']]
+            parts = []
+            for field in ['head', 'branch', 'tag', 'remote', 'stash']:
+                if field in ref:
+                    part = ' '.join(ref[field])
+                    color = cfg[f'{field}_color']
+                    if color != COLORS['default']:
+                        part = color + part + '\033[39m'
+                    parts.append(part)
+
+            commit['refs'] = ' '.join(parts)
+            if cfg['refs_bold']:
+                commit['refs'] = '\033[1m' + commit['refs'] + '\033[22m'
+            commit['refs'] += ' '
+
         for field in ['date', 'author', 'hash']:
             color = cfg[f'{field}_color']
-            if color and color != COLORS['default']:
+            if color != COLORS['default']:
                 commit[field] = (
                     color + commit[field] + COLORS['default']
                     + ' ' * (lengths[field] - len(commit[field])))
@@ -142,6 +210,12 @@ def parse_arguments():
         default='default',
         metavar='')
     parser.add_argument(
+        '--ntig-date-fmt',
+        help='date format',
+        dest='date_fmt',
+        default='%d %b %H:%M',
+        metavar='')
+    parser.add_argument(
         '--ntig-author',
         help='author name color',
         dest='author_color',
@@ -153,17 +227,53 @@ def parse_arguments():
         dest='node_color',
         default='bright-yellow',
         metavar='')
+
     parser.add_argument(
-        '--ntig-date-fmt',
-        help='date format',
-        dest='date_fmt',
-        default='%d %b %H:%M',
+        '--ntig-head',
+        help='HEAD color',
+        dest='head_color',
+        default='bright-cyan',
         metavar='')
+    parser.add_argument(
+        '--ntig-branch',
+        help='local branch color',
+        dest='branch_color',
+        default='bright-green',
+        metavar='')
+    parser.add_argument(
+        '--ntig-remote',
+        help='remote branch color',
+        dest='remote_color',
+        default='bright-red',
+        metavar='')
+    parser.add_argument(
+        '--ntig-tag',
+        help='tag color',
+        dest='tag_color',
+        default='bright-yellow',
+        metavar='')
+    parser.add_argument(
+        '--ntig-stash',
+        help='stash color',
+        dest='stash_color',
+        default='bright-blue',
+        metavar='')
+    parser.add_argument(
+        '--ntig-refs-bold',
+        help='show refs in bold',
+        action='store_true',
+        dest='refs_bold')
+    parser.add_argument(
+        '--ntig-decorate',
+        help='decorate refs',
+        action='store_true',
+        dest='decorate')
+
     parser.add_argument(
         '--ntig-log',
         help='log format',
         dest='log_fmt',
-        default='{hash} {date} {author} {graph} {message}',
+        default='{hash} {date} {author} {graph} {refs}{message}',
         metavar='')
     parser.add_argument(
         '--ntig-pager',
@@ -181,11 +291,15 @@ def parse_arguments():
     else:
         cfg['pager'] = shlex.split(cfg['pager'])
 
-    for field in ['hash_color', 'date_color', 'author_color', 'node_color']:
+    for field in ['hash_color', 'date_color', 'author_color', 'node_color',
+                  'head_color', 'branch_color', 'remote_color',
+                  'tag_color', 'stash_color']:
         color = cfg[field]
+        if not color.strip():
+            color = 'default'
         if color[0:4] == '\\033':
             color = '\033' + color[4:]
-        if color and color[0:1] != '\033':
+        if color[0:1] != '\033':
             if color in COLORS:
                 color = COLORS[color]
             else:
